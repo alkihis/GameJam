3e36000ebd42dec9d44ec698a67f44c5db273162
wydrop <theo.murgier@orange.fr>
2020-02-08 09:22:52 +0100

    Added Hunter.fbx. ADDED HUNTER PREFAB. Added different Hunter animation and animation controller
    jul ovni

---
+++ Assets/Scripts/CreateMaterial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+public class CreateMaterial : MonoBehaviour
+{
+    [MenuItem("Assets/Create Material with Texture")]
+    static void CMT()
+    {
+        string SelectedTexturePath = AssetDatabase.GetAssetPath(Selection.activeObject);
+        int i = SelectedTexturePath.LastIndexOf("/");
+        string FolderPath = i != -1 ? SelectedTexturePath.Substring(0, i + 1) : throw new Exception("Alien path!");
+        Material material = new Material(Shader.Find("Standard (Specular setup)"))
+        {
+            name = Selection.activeObject.name,
+            mainTexture = Selection.activeObject as Texture
+        };
+        material.SetFloat("_Glossiness", 0f);
+        material.SetColor("_SpecColor", Color.black);
+        AssetDatabase.CreateAsset(material, FolderPath + Selection.activeObject.name + ".mat");
+    }
+
+    [MenuItem("Assets/Create Material with Texture", true)]
+    static bool ValidateContextMenuItem()
+    {
+        return Selection.activeObject is Texture;
+    }
+}
