e76742696a9db12bc550ea806ef0d880264e7cf0
Azarias Boutin <azarias.boutin@gmail.com>
2020-02-08 17:24:40 +0100

    Introduction text

---
+++ Assets/Scripts/GamePlayManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePlayManager : MonoBehaviour
+{
+
+    public GameObject IntroCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IntroCanvas.SetActive(true);
+        var btn = GameObject.Find("OkButton");
+        btn.GetComponent<Button>().onClick.AddListener(() =>
+        {
+            IntroCanvas.SetActive(false);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/OkButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OkButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() =>
+        {
+            
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
