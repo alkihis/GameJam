009ff57010e18faaf1ba7b63050c32a45e5f7a8f
wydrop <theo.murgier@orange.fr>
2020-02-08 01:21:25 +0100

    Salut mon pote
    Ajout des dossiers Models. Exterior Materials. Ajout office.fbx

---
+++ Assets/Scripts/RotateSky.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class RotateSky : MonoBehaviour
+{
+    public float RotateSpeed = 1.2f;
+
+#if UNITY_EDITOR
+    private void OnDestroy()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", 0);
+    }
+
+    private void OnApplicationQuit()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", 0);
+    }
+#endif
+
+    private void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotateSpeed);
+    }
+}
