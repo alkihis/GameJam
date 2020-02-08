b7ceaa3193f3b36d2135f336ecd6dab2b92bebc0
Axsiow <Axsiow@gmail.com>
2020-02-08 13:34:43 +0100

    Add button menu

---
+++ Assets/Scripts/Axsiow/GoToCredits.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToCredits : MonoBehaviour{
+
+    // OK BOOMER
+    /*void Start () {
+        GetComponent<Button> ().onClick.AddListener (Back);
+    }
+
+    private void Back () {
+        SceneManager.LoadScene ("MainCredits");
+    }*/
+}
